184487cf8d822f8af2ba2dd159ad08c6a328b1be
stepanleas <s.leas@developmentaid.org>
2025-01-21 19:35:27 +0200

    add rpc implementation of create and login user

---
+++ gapi/rpc_create_user.go
@@ -0,0 +1,44 @@
+package gapi
+
+import (
+	"context"
+
+	"github.com/lib/pq"
+	db "github.com/stepanleas/backend-master-class/db/sqlc"
+	"github.com/stepanleas/backend-master-class/pb"
+	"github.com/stepanleas/backend-master-class/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
+			}
+		}
+
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	resp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+
+	return resp, nil
+}
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/stepanleas/backend-master-class/api"
 	db "github.com/stepanleas/backend-master-class/db/sqlc"
+	"github.com/stepanleas/backend-master-class/gapi"
+	"github.com/stepanleas/backend-master-class/pb"
 	"github.com/stepanleas/backend-master-class/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	_ "github.com/lib/pq"
 )
@@ -23,12 +28,38 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
